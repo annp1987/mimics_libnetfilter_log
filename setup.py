db2f1d2beb291cd49a06d3a84e91661ae07def65
Nguyen Phuong An <annp1987@users.noreply.github.com>
2017-09-05 10:03:15 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='mimisc_libnetfilter_log',
+setup(name='mimics_libnetfilter_log',
       version='1.0',
       description='Python libnetfilter_log ctypes',
       author='Nguyen Phuong An',
       author_email='annp.cs51@gmail.com',
-      url='https://github.com/annp1987/mimisc_libnetfilter_log',
+      url='https://github.com/annp1987/mimics_libnetfilter_log',
       packages=['lib', 'lib.netlink', 'lib.log']
 )
